d1549350bc2ca043a4fbfd543dd4219445655211
Вадим <kvady@mail.ru>
2024-07-17 15:00:33 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,10 @@
+# homework4.py
+# Организация программы
+my_string = input('Введите Ф. И. О.: ')
+print('Количество символов: ',len(my_string))
+# Работа с методами строк
+print(my_string.upper())
+print(my_string.upper().lower())
+print(my_string.replace(' ','$' ))
+print(my_string [0])
+print(my_string [-1])
