8edb8e4fb29cb1b2466b5fb3606bba8c424ba8bc
Вадим <kvady@mail.ru>
2024-07-22 01:58:07 +0300

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,33 @@
+print("Ввод в консоль 1:")
+first = int(input())
+second = int(input())
+third = int(input())
+print("Вывод в консоль 1:")
+if first == second and second == third and first == third:
+    print(3)
+elif first == second or second == third or first == third:
+    print(2)
+else:
+    print(0)
+print("Ввод в консоль 2:")
+first = int(input())
+second = int(input())
+third = int(input())
+print("Вывод в консоль 2:")
+if first == second == third:
+    print(3)
+elif first == second or second == third or first == third:
+    print(2)
+else:
+    print(0)
+print("Ввод в консоль 3:")
+first = int(input())
+second = int(input())
+third = int(input())
+print("Вывод в консоль 3:")
+if first == second and second == third and first == third:
+    print(3)
+elif first == second or second == third or first == third:
+    print(2)
+else:
+    print(0)
