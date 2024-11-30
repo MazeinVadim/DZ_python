e244af83b016bbd5988603b4b8c3c1bdcdeb22ae
Вадим <kvady@mail.ru>
2024-11-30 22:38:35 +0300

    Add files via upload

--- crud_functions.py
+++ crud_functions.py
@@ -13,6 +13,17 @@ def initiate_db():
             image_url TEXT
         )
     ''')
+
+ # Создание таблицы Users, если она еще не создана
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS Users (
+            id INTEGER PRIMARY KEY,
+            username TEXT NOT NULL,
+            email TEXT NOT NULL,
+            age INTEGER NOT NULL,
+            balance INTEGER NOT NULL DEFAULT 1000
+        )
+    ''')
     conn.commit()
     conn.close()
 
@@ -23,3 +34,21 @@ def get_all_products():
     products = cursor.fetchall()
     conn.close()
     return products
+
+def add_user(username, email, age):
+    conn = sqlite3.connect('products.db')
+    cursor = conn.cursor()
+    cursor.execute('''
+        INSERT INTO Users (username, email, age, balance)
+        VALUES (?, ?, ?, 1000)
+    ''', (username, email, age))
+    conn.commit()
+    conn.close()
+
+def is_included(username):
+    conn = sqlite3.connect('products.db')
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM Users WHERE username = ?', (username,))
+    user = cursor.fetchone()
+    conn.close()
+    return user is not None
