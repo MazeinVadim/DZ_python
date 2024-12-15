d48b04a8a2033dd4751de7fd85802ff39e78bac1
Вадим <kvady@mail.ru>
2024-12-15 05:24:40 +0300

    Add files via upload

---
+++ module_16_1.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI
+
+# Создание объекта FastAPI
+app = FastAPI()
+
+# Создание маршрута к главной странице "/"
+@app.get("/")
+def home():
+    return "Главная страница"
+
+# Создание маршрута к странице администратора "/user/admin"
+@app.get("/user/admin")
+def admin_page():
+    return "Вы вошли как администратор"
+
+# Маршрут к странице пользователя с параметром user_id
+@app.get("/user/{user_id}")
+def user_page(user_id: int):
+    return f"Вы вошли как пользователь № 'Kirill'"
+
+# Маршрут к странице пользователя с параметрами username и age в адресной строке
+@app.get("/user")
+def user_info(username: str, age: int):
+    return f"Информация о пользователе. Имя: 'Vadim', Возраст: '37'"
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
